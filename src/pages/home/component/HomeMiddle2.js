55ce979f90ce9a5a281ba6f394a52c61450a1c39
vambknight <vambknight@gmail.com>
2023-02-15 21:23:13 +0800

    feat: code update

---
+++ src/pages/home/component/HomeMiddle2.js
@@ -0,0 +1,69 @@
+import React from 'react'
+import styled from "styled-components";
+
+const HomeMiddle2 = () => {
+  return (
+    <Wrapper>
+      <div className={'logo-unit'} >
+        <img src={require('../../assets/logos/0.jpg')} alt={''} />
+      </div>
+      <div className={'logo-unit'} >
+        <img src={require('../../assets/logos/1.jpg')} alt={''} />
+      </div>
+      <div className={'logo-unit'} >
+        <img src={require('../../assets/logos/2.jpg')} alt={''} />
+      </div>
+      <div className={'logo-unit'} >
+        <img src={require('../../assets/logos/3.jpg')} alt={''} />
+      </div>
+      <div className={'logo-unit'} >
+        <img src={require('../../assets/logos/4.jpg')} alt={''} />
+      </div>
+      <div className={'logo-unit'} >
+        <img src={require('../../assets/logos/5.jpg')} alt={''} />
+      </div>
+      <div className={'logo-unit'} >
+        <img src={require('../../assets/logos/6.jpg')} alt={''} />
+      </div>
+      <div className={'logo-unit'} >
+        <img src={require('../../assets/logos/7.jpg')} alt={''} />
+      </div>
+      <div className={'logo-unit'} >
+        <img src={require('../../assets/logos/8.jpg')} alt={''} />
+      </div>
+      <div className={'logo-unit'} >
+        <img src={require('../../assets/logos/9.jpg')} alt={''} />
+      </div>
+    </Wrapper>
+  )
+}
+
+const Wrapper = styled('div')`
+  @keyframes unitHover {
+    0% { filter: grayscale(1); transform: scale(1); }
+    100% { filter: grayscale(0); transform: scale(1.1); }
+  }
+  @keyframes unitImHover {
+    0% { filter: grayscale(0); transform: scale(1.1); }
+    100% { filter: grayscale(1); transform: scale(1); }
+  }
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  width: 100%;
+  grid-gap: 10px;
+  .logo-unit {
+    width: 100%;
+    overflow: hidden;
+    border-radius: 6px;
+    & > img {
+      width: 100%;
+      cursor: pointer;
+      animation: unitImHover 0.1s steps(100) 0s 1 normal forwards running;
+    }
+    & > img:hover {
+      animation: unitHover 0.1s steps(100) 0s 1 normal forwards running;
+    }
+  }
+`
+
+export default HomeMiddle2
--- src/pages/home/homePage.js
+++ src/pages/home/homePage.js
@@ -1,24 +1,30 @@
 import React from 'antd'
 import styled from "styled-components";
-import ark_caster from '../arkSVGs/ark_caster.svg'
+import ark_caster from '../assets/arkSVGs/ark_caster.svg'
 import HomeMiddle from "./component/HomeMiddle";
+import HomeConstant from "./HomeConstant";
+import HomeMiddle2 from "./component/HomeMiddle2";
 
 const HomePage = () => {
   return (
     <Wrapper>
       <div className={'header-content'}>
         <div className={'info-content'}>
-          <div className={'info-title'}>阿米娅&nbsp;<img src={ark_caster} alt={''} /></div>
-          <div className={'info-sub-title'}>
-            <div>AMIYA</div>
-            <div>CV:黑泽明世</div>
+          <div className={'info-title'}>
+            {HomeConstant.HEADER_CONTEXT.INFO_DATA.title}&nbsp;
+            <img src={ark_caster} alt={''} />
           </div>
-          <div className={'info-desc'}>罗德岛的公开领袖，在内部拥有最高执行权。虽然，从外表上看起来仅仅是个不成熟的少女，实际上，她却是深受大家信任的合格的领袖。现在，阿米娅正带领着罗德岛，为了感染者的未来，为了让这片大地挣脱矿石病的阴霾而不懈努力。</div>
+          <div className={'info-sub-title'}>
+            <div>{HomeConstant.HEADER_CONTEXT.INFO_DATA.subTitle1}</div>
+            <div>{HomeConstant.HEADER_CONTEXT.INFO_DATA.subTitle2}</div>
+          </div>
+          <div className={'info-desc'}>{HomeConstant.HEADER_CONTEXT.INFO_DATA.detail}</div>
         </div>
         <div className={'img-content'}>
-          <img src={require('../arkAgent/casterAMiYa.png')} />
+          <img src={require('../assets/arkAgent/casterAMiYa.png')} alt={''} />
         </div>
       </div>
+      <HomeMiddle2 />
       <HomeMiddle />
     </Wrapper>
   )
